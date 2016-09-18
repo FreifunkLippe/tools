28e72e711c5965d58fb79d6b773c303b68ffabff
Tronde <tronde@my-it-brain.de>
2016-09-18 10:25:41 +0200

    Added variable for error log.

--- build_local_archives.sh
+++ build_local_archives.sh
@@ -7,6 +7,7 @@
 # Variables ##################################################################
 IMAGE_PATH="/var/www/jkastning/sites/firmware.freifunk-lippe.de/public/images"
 ARCHIVE_PATH="/var/www/jkastning/sites/firmware.freifunk-lippe.de/public/Archives"
+ERROR_LOG=""
 VERSION="0.8.14"
 D1=( ex ff lip )
 D2=( ad dt hb la )
@@ -29,8 +30,8 @@ for town in "${D1[@]}"
     then
       /bin/rm "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip
     fi
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d1/"${town}"/factory/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d1/"${town}"/sysupgrade/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d1/"${town}"/factory/*.bin > /dev/null 2> "${ERROR_LOG}"
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d1/"${town}"/sysupgrade/*.bin > /dev/null 2> "${ERROR_LOG}"
 done
 
 for town in "${D2[@]}"
@@ -43,8 +44,8 @@ for town in "${D2[@]}"
     then
       /bin/rm "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip
     fi
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d2/"${town}"/factory/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d2/"${town}"/sysupgrade/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d2/"${town}"/factory/*.bin > /dev/null 2> "${ERROR_LOG}"
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d2/"${town}"/sysupgrade/*.bin > /dev/null 2> "${ERROR_LOG}"
 done
 
 for town in "${D3[@]}"
@@ -57,8 +58,8 @@ for town in "${D3[@]}"
     then
       /bin/rm "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip
     fi
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d3/"${town}"/factory/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d3/"${town}"/sysupgrade/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d3/"${town}"/factory/*.bin > /dev/null 2> "${ERROR_LOG}"
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d3/"${town}"/sysupgrade/*.bin > /dev/null 2> "${ERROR_LOG}"
 done
 
 for town in "${D4[@]}"
@@ -71,6 +72,6 @@ for town in "${D4[@]}"
     then
       /bin/rm "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip
     fi
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d4/"${town}"/factory/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
-    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d4/"${town}"/sysupgrade/*.bin > /dev/null 2> ~/fflip-fw/fehler.txt
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_factory_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d4/"${town}"/factory/*.bin > /dev/null 2> "${ERROR_LOG}"
+    /usr/bin/zip -j "${ARCHIVE_PATH}/All_sysupgrade_images_for_${town}_${VERSION}".zip "${IMAGE_PATH}"/d4/"${town}"/sysupgrade/*.bin > /dev/null 2> "${ERROR_LOG}"
 done
